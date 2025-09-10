f41f93d71e922c2bc0f1f02c1355e99f4680964a
MehediAlam49 <mehedialam806@gmail.com>
2025-09-10 23:08:13 +0600

    add functions demonstration script

---
+++ Simplilearn-Python for Beginners/functions.py
@@ -0,0 +1,14 @@
+import math
+
+x = -7.5
+print(abs(x))
+
+y = 10
+print(math.exp(y))
+
+
+print(f"The value of e is:{math.e}")
+print(f"The value of PI is:{math.pi}")
+print(f"The value of sqrt value is:{math.sqrt(6)}")
+print(f"The maximum value of the list is:{max(1, 151,23563,622)}")
+print(f"The minimum value of the list is:{min(1, 151,23563,622)}")
