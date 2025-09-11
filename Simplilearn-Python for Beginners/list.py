f24a22ae3a0ab9c5bb48c6559e9b1b93c8f14c88
MehediAlam49 <mehedialam806@gmail.com>
2025-09-11 10:09:46 +0600

    update list.py to include additional elements in the mix list and enhance element access demonstrations

--- Simplilearn-Python for Beginners/list.py
+++ Simplilearn-Python for Beginners/list.py
@@ -11,9 +11,28 @@ str = ['mehedi','alam','saiful','alam']
 print(str)
 
 # Mixing list
-mix = ['mehedi',49, True, 'simplilearn']
+mix = ['mehedi',49, True, 'simplilearn','course']
 print(mix)
 
 # matrix list
 mat = [[1,2],[3,4],[8,9]]
 print(mat)
+
+
+
+# -----Access of Elements------
+
+# length of list
+print(len(mix))
+
+# Access element using index
+print(mix[:3])
+# Access element using reverse index
+print(mix[3:])
+# Access element using rang of index
+
+print(mix[2:4])
+# Access element every 2nd index
+print(mix[::2])
+# Access element using reverse index
+print(mix[::-1])
