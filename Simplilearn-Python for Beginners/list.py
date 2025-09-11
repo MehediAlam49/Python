71cb66d30f7b013b90eabd27331561583a2f24cb
MehediAlam49 <mehedialam806@gmail.com>
2025-09-11 09:52:32 +0600

    add list demonstration script

---
+++ Simplilearn-Python for Beginners/list.py
@@ -0,0 +1,19 @@
+# number list
+num = [1,3,5,6,0]
+print(num)
+
+# letter list
+letter = ['m','e','h','e','d','i']
+print(letter)
+
+# string list
+str = ['mehedi','alam','saiful','alam']
+print(str)
+
+# Mixing list
+mix = ['mehedi',49, True, 'simplilearn']
+print(mix)
+
+# matrix list
+mat = [[1,2],[3,4],[8,9]]
+print(mat)
