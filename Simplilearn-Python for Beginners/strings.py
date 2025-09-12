8913969fe0b1d1035e0c9772ae2419a401b2c87d
MehediAlam49 <mehedialam806@gmail.com>
2025-09-12 12:39:25 +0600

    add: create strings.py with examples of string manipulation and methods

---
+++ Simplilearn-Python for Beginners/strings.py
@@ -0,0 +1,20 @@
+# string with a single quotation
+x="Hi! I'm Mehedi Alam"
+print(x)
+# string with a double quotation
+x="Teacher said, \"this is your hw\""
+print(x)
+
+# slice method
+str = "Welcome to here"
+print(str[8:10])
+
+# uppercase method
+print(str.upper())
+
+# lowercase method
+print(str.lower())
+
+
+
+
