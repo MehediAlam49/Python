5ddc815b1551cb11ffe7bae4b536338ddbbb2b3c
MehediAlam49 <mehedialam806@gmail.com>
2025-09-13 10:53:23 +0600

    add: create sets.py with examples of add and remove operations on a set

---
+++ sets.py
@@ -0,0 +1,10 @@
+fruits = {"apple", "banana", "cherry", "apple"}
+print(fruits)
+
+# add operation
+fruits.add('guava')
+print(fruits)
+
+#remove operation
+fruits.remove('banana') #Removes an element (error if missing)
+print(fruits)
