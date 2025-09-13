11bc0e45fdbf4d3bb8d2407a410b31cad48d48a6
MehediAlam49 <mehedialam806@gmail.com>
2025-09-13 13:32:41 +0600

    update for-loop.py to add sum calculation of even numbers using range

--- Simplilearn-Python for Beginners/for-loop.py
+++ Simplilearn-Python for Beginners/for-loop.py
@@ -10,3 +10,10 @@ for i in range(0,21):
 # range method for print 0-20 in after a number
 for i in range(0,21,2):
     print(i)
+    
+    
+# sum of number using range
+total = 0
+for i in range(0, 21, 2):
+    total += i
+print(f"The total sum of even numbers from 0 to 20 is {total}")
