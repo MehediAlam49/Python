341d6beaf51fd46aead556c2ab0418c5a5810ce2
MehediAlam49 <mehedialam806@gmail.com>
2025-09-13 11:02:37 +0600

    update sets.py to add examples of union, intersection, and difference operations

--- sets.py
+++ sets.py
@@ -20,3 +20,15 @@ print(fruits)
 # clear operation
 fruits.clear()
 print(fruits)
+
+
+# union operation
+s1 = {1, 2, 3,8,7}
+s2 = {2,5,8,0,9}
+print(s1.union(s2)) #Combines sets
+
+# intersection operation
+print(s1.intersection(s2)) #Common elements
+
+# difference operation
+print(s1.difference(s2)) # Elements in set1 not in set2
