d800c0d918f458491904be044af1c1bc0e4593ec
MehediAlam49 <mehedialam806@gmail.com>
2025-09-13 13:28:57 +0600

    add for-loop.py with examples of basic for loops and range usage

---
+++ Simplilearn-Python for Beginners/for-loop.py
@@ -0,0 +1,12 @@
+x = [3,8,'Mehedi']
+for i in x:
+    print(i)
+    
+# range method for print 0-20
+for i in range(0,21):
+    print(i)
+    
+    
+# range method for print 0-20 in after a number
+for i in range(0,21,2):
+    print(i)
