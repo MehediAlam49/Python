8dc4d3c185127159584983a69dc4d48326897942
MehediAlam49 <mehedialam806@gmail.com>
2025-09-13 10:56:46 +0600

    update sets.py to include examples of discard, pop, and clear operations

--- sets.py
+++ sets.py
@@ -8,3 +8,15 @@ print(fruits)
 #remove operation
 fruits.remove('banana') #Removes an element (error if missing)
 print(fruits)
+
+#discard operation
+fruits.discard('banana') # Removes an element (no error)
+print(fruits)
+
+# pop operations
+fruits.pop() #Removes and returns a random item
+print(fruits)
+
+# clear operation
+fruits.clear()
+print(fruits)
