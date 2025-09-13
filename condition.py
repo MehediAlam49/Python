e72720b4281fcbf794a24ccc75e759f26ba517a3
MehediAlam49 <mehedialam806@gmail.com>
2025-09-13 11:31:32 +0600

    add: create condition.py with examples of if, if-else, and nested if statements

---
+++ condition.py
@@ -0,0 +1,23 @@
+# if statement
+a = 30
+
+if a>=18:
+    print('You are voter')
+    
+    
+# if-else statement
+x = 25
+if x%2==0:
+    print('x is a even number')
+else:
+    print('x is odd number')
+    
+#nested if statement
+z = 33
+if z>25:
+    if z%2 == 0:
+        print('Number is grater then 25 and even number')
+    else:
+        print('Number is grater then 25 and odd number')
+else:
+    print('Number is less then 25')
