b8cbb98d0774aeb69a0970c73be45ade74749909
MehediAlam49 <mehedialam806@gmail.com>
2025-09-13 13:00:20 +0600

    update loop.py to add examples of while, for, and nested loops

--- Simplilearn-Python for Beginners/loop.py
+++ Simplilearn-Python for Beginners/loop.py
@@ -1,5 +1,19 @@
+
+# while loop
 num = int(input("Enter a number that is a multiple of 7: "))
 while num % 7 != 0:
     print(f"{num} is not a multiple of 7.")
     num = int(input("Try again: "))
 print(f"{num} is a multiple of 7!")
+
+# for loop
+z = [3,8,'mehedi','saiful']
+for i in z:
+    print(i)
+    
+# nested loop 
+x = [[1,3,56],['a','b','c']]
+for i in x:
+    for j in i:
+        print(j, end='')
+    print()
