3ddfd655346330442357eb8428652e51eb36e53f
MehediAlam49 <mehedialam806@gmail.com>
2025-09-27 15:08:02 +0600

    add nested_called_function.py to demonstrate nested function calls

---
+++ Simplilearn-Python for Beginners/nested_called_function.py
@@ -0,0 +1,9 @@
+def func1 (called_func) :
+    print ("This is the first function")
+    def nested_func1 (called_func) :
+        print ("This is the nested function")
+        called_func( )
+    return nested_func1 (called_func)
+def outer_func () :
+    print ("This is the outer function")
+func1(outer_func)
